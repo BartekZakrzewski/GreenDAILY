858ed4e15c95f2c7dbf7ec58d1de03b62abf1eed
Bartosz Zakrzewski <bartek.zakrzewski3012@gmail.com>
2024-10-29 20:53:54 +0100

    123

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import { Header } from "../components/index";
-
 const App = () => {
   return (
     <>
-      <Header />
+      <main className="h-screen flex items-center justify-center">
+        <div className="flex flex-col w-2/3 gap-5">
+          <h1 className="text-6xl font-extrabold leading-snug">
+            Incorporate <span className="text-green-900 italic">eco-friendly</span> habits into your daily life through recommendations, challenges and resources!
+          </h1>
+          <div className="flex items-center justify-center">
+            <a href="#" className="text-3xl text-white font-medium bg-black px-6 py-3 rounded-md">Try now</a>
+          </div>
+        </div>
+      </main>
+      <section>
+        
+      </section>
     </>
   );
 };
--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,13 @@
 const Header = () => {
   return (
-    <header className="w-full bg-red">
-      <h1>
-        Green<span className="">daily</span>
+    <header className="w-full flex justify-between p-2 border-b-2 border-gray-500/10">
+      <h1 className="text-2xl my-auto">
+        Green<span className="uppercase text-green-900 font-extrabold">daily</span>
       </h1>
+      <div className="grid grid-cols-2 items-center px-2">
+        <a href="#">Log in</a>
+      <a href="#" className="bg-green-900 py-3 px-5 items-center rounded-md text-white">Sign in</a>
+      </div>
     </header>
   );
 };
